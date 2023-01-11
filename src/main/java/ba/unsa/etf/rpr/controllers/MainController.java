b46a8ed5cc5aaf2e749dc2aa7b3bc1bcb3a1eebb
hpamuk <hpamuk1@etf.unsa.ba>
2023-01-11 15:42:38 +0100

    importi uradjeni

--- src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/MainController.java
@@ -1,21 +1,16 @@
 package ba.unsa.etf.rpr.controllers;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
 
 public class MainController {
     public Label label;
-    // submit, cancel
     public void submitClick(ActionEvent actionEvent) throws IOException {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle("Uspješan zahtjev!");
